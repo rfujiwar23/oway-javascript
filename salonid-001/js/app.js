d5f666c7de0695b6c0742d6b969a79dad21821c3
Ryo Fujiwara <jjcproject@icloud.com>
2021-11-04 15:29:04 +0900

    javascript html page

---
+++ salonid-001/js/app.js
@@ -0,0 +1,22 @@
+var vm = new Vue({
+    el: '#xVolume',
+    data: {
+      items: [
+        {productNameJP: 'ボリューム　ヘアバス 240ml', productNameENG: 'volumizing hair bath 240ml', imageSource: '../../image/individual/volumizing_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/23?salon_id=5'},
+        {productNameJP: 'ボリューム　ルーツスプレー 160ml', productNameENG: 'volumizing root spray 160ml', imageSource: '../../image/individual/volumizing_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/25?salon_id=5'}
+      ]
+    }
+});
+
+
+var vm = new Vue({
+  el: '#smooth',
+  data: {
+    smoothProducts: [
+      {productNameJP: 'ノンリンス スムージング フルイド 100ml', productNameENG: 'smoothing fluid 100ml', imageSource: '../../image/individual/smoothing_fluid_100ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/23?salon_id=5'},
+      {productNameJP: 'スムージング ヘアバス 240ml', productNameENG: 'smoothing hair bath 240ml', imageSource: '../../image/individual/smoothing_hair_bath_240ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/31?salon_id=5'},
+      {productNameJP: 'スムージング　コンディショナー 240ml', productNameENG: 'smoothing conditioner 240ml', imageSource: '../../image/individual/smoothing_conditioner_240ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/33?salon_id=5'},
+      {productNameJP: 'スムージング クリーム 150ml', productNameENG: 'smoothing cream 150ml', imageSource: '../../image/individual/smoothing_cream_150ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/35?salon_id=5'}
+    ]
+  }
+});
