6f4c924c645f0a4f0831339eee88e4fbdc059fdb
Ryo Fujiwara <jjcproject@icloud.com>
2021-11-06 15:55:18 +0900

    ordering cdn vue

--- salonid-001/js/app.js
+++ salonid-001/js/app.js
@@ -1,14 +1,63 @@
+// dailyAct
 var vm = new Vue({
-    el: '#xVolume',
-    data: {
-      items: [
-        {productNameJP: 'ボリューム　ヘアバス 240ml', productNameENG: 'volumizing hair bath 240ml', imageSource: '../../image/individual/volumizing_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/23?salon_id=5'},
-        {productNameJP: 'ボリューム　ルーツスプレー 160ml', productNameENG: 'volumizing root spray 160ml', imageSource: '../../image/individual/volumizing_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/25?salon_id=5'}
-      ]
-    }
+  el: '#dailyAct',
+  data: {
+    dailyItems: [
+      {productNameJP: 'デイリーユーズ　ヘアバス 240ml', productNameENG: 'dailyAct hair & scalp bath 240ml', imageSource: '../../image/individual/dailyAct_hair_bath_240ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/7?salon_id=5'},
+      {productNameJP: 'デイリーユーズ　コンディショナー 240ml', productNameENG: 'frequent use conditioner 240ml', imageSource: '../../image/individual/dailyAct_conditioner_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/9?salon_id=5'},
+      {productNameJP: 'デイリーユーズ　ミスト 160ml', productNameENG: 'dailyAct phytoprotein mist 160ml', imageSource: '../../image/individual/dailyAct_phytoprotein.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/11?salon_id=5'}
+    ]
+  }
 });
 
+// moisturizing
+var vm = new Vue({
+  el: '#moisturizing',
+  data: {
+    moistItems: [
+      {productNameJP: 'モイスト ヘアバス 240ml', productNameENG: 'moisturizing hair bath 240ml', imageSource: '../../image/individual/moisturizing_hair_bath_240ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/12?salon_id=5'},
+      {productNameJP: 'モイスト ヘアマスク 150ml', productNameENG: 'moisturizing hair mask 150ml', imageSource: '../../image/individual/moisturizing_hair_mask_150ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/15?salon_id=5'},
+      {productNameJP: 'ノンリンス モイスト ヘアバス 160ml', productNameENG: 'no rinse moist conditioner 160ml', imageSource: '../../image/individual/moisturizing_hair_moisture_conditioner.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/17?salon_id=5'}
+    ]
+  }
+});
 
+// ColorUp
+var vm = new Vue({
+  el: '#color',
+  data: {
+    colorItems: [
+      {productNameJP: 'カラープロテクション ヘアバス 240ml', productNameENG: 'color protection hair bath 240ml', imageSource: '../../image/individual/color_protection_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/18?salon_id=5'},
+      {productNameJP: 'カラープロテクション ヘアマスク 150ml', productNameENG: 'color protection hair mask 150ml', imageSource: '../../image/individual/color_protection_hair_mask_150ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/20?salon_id=5'},
+      {productNameJP: 'カラープロテクション ベール 160ml', productNameENG: 'color protection veil 160ml', imageSource: '../../image/individual/color_protection_veil_160ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/22?salon_id=5'}
+    ]
+  }
+});
+
+// xVolume
+var vm = new Vue({
+  el: '#xVolume',
+  data: {
+    items: [
+      {productNameJP: 'ボリューム　ヘアバス 240ml', productNameENG: 'volumizing hair bath 240ml', imageSource: '../../image/individual/volumizing_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/23?salon_id=5'},
+      {productNameJP: 'ボリューム　ルーツスプレー 160ml', productNameENG: 'volumizing root spray 160ml', imageSource: '../../image/individual/volumizing_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/25?salon_id=5'}
+    ]
+  }
+});
+
+// beCurly
+var vm = new Vue({
+  el: '#curly',
+  data: {
+    curlyItems: [
+      {productNameJP: 'カーリー　ヘアバス 240ml', productNameENG: 'curly hair bath 240ml', imageSource: '../../image/individual/curly_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/26?salon_id=5'},
+      {productNameJP: 'カーリー　ヘアマスク 150ml', productNameENG: 'curly hair mask 150ml', imageSource: '../../image/individual/curly_hair_mask_150ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/28?salon_id=5'},
+      {productNameJP: 'カーリー　ポーション 240ml', productNameENG: 'curly potion 240ml', imageSource: '../../image/individual/curly_potion.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/30?salon_id=5'}
+    ]
+  }
+});
+
+// Smooth
 var vm = new Vue({
   el: '#smooth',
   data: {
@@ -21,6 +70,7 @@ var vm = new Vue({
   }
 });
 
+// Rebuilding
 var vm = new Vue({
   el: '#rebuilding',
   data: {
@@ -32,46 +82,22 @@ var vm = new Vue({
   }
 });
 
+// mens
 var vm = new Vue({
-  el: '#color',
+  el: '#mens',
   data: {
-    colorItems: [
-      {productNameJP: 'カラープロテクション ヘアバス 240ml', productNameENG: 'color protection hair bath 240ml', imageSource: '../../image/individual/color_protection_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/18?salon_id=5'},
-      {productNameJP: 'カラープロテクション ヘアマスク 150ml', productNameENG: 'color protection hair mask 150ml', imageSource: '../../image/individual/color_protection_hair_mask_150ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/20?salon_id=5'},
-      {productNameJP: 'カラープロテクション ベール 160ml', productNameENG: 'color protection veil 160ml', imageSource: '../../image/individual/color_protection_veil_160ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/22?salon_id=5'}
-    ]
-  }
-});
-
-var vm = new Vue({
-  el: '#curly',
-  data: {
-    curlyItems: [
-      {productNameJP: 'カーリー　ヘアバス 240ml', productNameENG: 'curly hair bath 240ml', imageSource: '../../image/individual/curly_hair_bath_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/26?salon_id=5'},
-      {productNameJP: 'カーリー　ヘアマスク 150ml', productNameENG: 'curly hair mask 150ml', imageSource: '../../image/individual/curly_hair_mask_150ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/28?salon_id=5'},
-      {productNameJP: 'カーリー　ポーション 240ml', productNameENG: 'curly potion 240ml', imageSource: '../../image/individual/curly_potion.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/30?salon_id=5'}
-    ]
-  }
-});
-
-var vm = new Vue({
-  el: '#dailyAct',
-  data: {
-    dailyItems: [
-      {productNameJP: 'デイリーユーズ　ヘアバス 240ml', productNameENG: 'dailyAct hair & scalp bath 240ml', imageSource: '../../image/individual/dailyAct_hair_bath_240ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/7?salon_id=5'},
-      {productNameJP: 'デイリーユーズ　コンディショナー 240ml', productNameENG: 'frequent use conditioner 240ml', imageSource: '../../image/individual/dailyAct_conditioner_240ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/9?salon_id=5'},
-      {productNameJP: 'デイリーユーズ　ミスト 160ml', productNameENG: 'dailyAct phytoprotein mist 160ml', imageSource: '../../image/individual/dailyAct_phytoprotein.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/11?salon_id=5'}
-    ]
-  }
-});
-
-var vm = new Vue({
-  el: '#moisturizing',
-  data: {
-    moistItems: [
-      {productNameJP: 'モイスト ヘアバス 240ml', productNameENG: 'moisturizing hair bath 240ml', imageSource: '../../image/individual/moisturizing_hair_bath_240ml_F.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/12?salon_id=5'},
-      {productNameJP: 'モイスト ヘアマスク 150ml', productNameENG: 'moisturizing hair mask 150ml', imageSource: '../../image/individual/moisturizing_hair_mask_150ml.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/15?salon_id=5'},
-      {productNameJP: 'ノンリンス モイスト ヘアバス 160ml', productNameENG: 'no rinse moist conditioner 160ml', imageSource: '../../image/individual/moisturizing_hair_moisture_conditioner.jpg', ifingUrl: 'https://ifing-beauty.com/products/detail/17?salon_id=5'}
+    mensItems: [
+      {productNameJP: 'フェイス＆ビアード ハイドレイティング・クレンザー', productNameENG: 'face & beard hydrating cleanser', imageSource: '../../image/product_men/001_face-&-beard-hydrating-cleanser.png', ifingUrl: 'https://ifing-beauty.com/products/detail/109?salon_id=5'},
+      {productNameJP: 'ソフニング・シェイビング・クリーム', productNameENG: 'softening shaving cream', imageSource: '../../image/product_men/002_softening-shave-cream.png', ifingUrl: 'https://ifing-beauty.com/products/detail/112?salon_id=5'},
+      {productNameJP: 'アフターシェイビング・バーム', productNameENG: 'repairing aftershave balm', imageSource: '../../image/product_men/003_repairing-after-shave.png', ifingUrl: 'https://ifing-beauty.com/products/detail/113?salon_id=5'},
+      {productNameJP: 'フェイス＆アイ エナジー・テクスチャー', productNameENG: 'face & eye energizing texture', imageSource: '../../image/product_men/006_face-&-eye-energizing-texture.png', ifingUrl: 'https://ifing-beauty.com/products/detail/110?salon_id=5'},
+      {productNameJP: 'リップ・ビアード・サルブ', productNameENG: 'Lip & Beard Salve', imageSource: '../../image/product_men/009_lip-&-beard.png', ifingUrl: 'https://ifing-beauty.com/products/detail/114?salon_id=5'},
+      {productNameJP: 'ヘア＆ボディ リフレッシング・ウォッシュ', productNameENG: 'hair & body invigorating wash', imageSource: '../../image/product_men/004_hair-and-body-invigorating-wash.png', ifingUrl: 'https://ifing-beauty.com/products/detail/111?salon_id=5'},
+      {productNameJP: 'ウォーター・レジン', productNameENG: 'water resin', imageSource: '../../image/product_men/011_water-resin.png', ifingUrl: 'https://ifing-beauty.com/products/detail/105?salon_id=5'},
+      {productNameJP: 'ロー・マッド', productNameENG: 'raw mud', imageSource: '../../image/product_men/008_raw-mud.png', ifingUrl: 'https://ifing-beauty.com/products/detail/106?salon_id=5'},
+      {productNameJP: 'バンジー・ファイバー', productNameENG: 'bungee fiber', imageSource: '../../image/product_men/010_bungee-fiber.png', ifingUrl: 'https://ifing-beauty.com/products/detail/107?salon_id=5'},
+      {productNameJP: 'ノクターナル・ヘア・レメディ', productNameENG: 'nocturnal hair remedy', imageSource: '../../image/product_men/005_nocturnal-hair-remedy.png', ifingUrl: 'https://ifing-beauty.com/products/detail/211?salon_id=5'},
+      {productNameJP: 'シルバー・スティール・ヘアバス', productNameENG: 'silver steel hair bath', imageSource: '../../image/product_men/007_silver-steel-hair-bath.png', ifingUrl: 'https://ifing-beauty.com/products/detail/108?salon_id=5'},
     ]
   }
 });
